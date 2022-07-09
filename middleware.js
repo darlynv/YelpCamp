d016a5fcf1ba15a69f36fd8490cf909cc9140603
Darlyn Vasquez <vas.darlyn@gmail.com>
2022-07-09 14:16:00 -0400

    fixed review deleting bug

--- middleware.js
+++ middleware.js
@@ -23,7 +23,7 @@ module.exports.validateReview = (req, res, next) => {
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400);
-    }  else {
+    } else {
         next();
     }
 };
@@ -49,7 +49,7 @@ module.exports.isAuthor = async (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
-    const review = await Review.findById(id);
+    const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
